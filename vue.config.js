c0e9e49aa0039f03557bad6032bdfa8c70b40a3d
Jocian de Souza Mendonça <jocianmendonca@gmail.com>
2020-07-25 16:13:04 +0100

    Feat: add electron

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    electronBuilder: {
+      outputDir: 'dist/electron',
+      appId: 'com.icore.dev-browser',
+      copyright: 'Copyright © 2020 by Jocian de Souza Mendonça',
+      asar: true
+    }
+  }
 }
