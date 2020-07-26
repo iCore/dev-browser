8b04a10c1ca1ae46c27650afe3ca7cae0806438f
Jocian de Souza Mendonça <jocianmendonca@gmail.com>
2020-07-26 14:20:38 +0100

    Feat: clear project and create system bar

--- src/background.js
+++ src/background.js
@@ -2,8 +2,8 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import { autoUpdater } from 'electron-updater'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -19,46 +19,54 @@ protocol.registerSchemesAsPrivileged([
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 400,
+    minHeight: 300,
+
+    useContentSize: true,
+    center: true,
+    show: false,
+    frame: false,
+
+    backgroundColor: '#313131',
+
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      webviewTag: true
     }
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
+
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
+
     autoUpdater.checkForUpdatesAndNotify()
   }
 
-  win.on('closed', () => {
-    win = null
-  })
+  win.on('ready-to-show', () => win && win.show())
+
+  win.on('closed', () => (win = null))
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+  if (win === null) createWindow()
 })
 
 // This method will be called when Electron has finished
@@ -73,6 +81,7 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
+
   createWindow()
 })
 
@@ -80,9 +89,7 @@ app.on('ready', async () => {
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
+      if (data === 'graceful-exit') app.quit()
     })
   } else {
     process.on('SIGTERM', () => {
---
+++ src/components/system-bar.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: 'SystemBar'
+}
+</script>
+
+<template>
+  <v-system-bar id="system-bar" app>
+    <span>Dev Browser</span>
+    <v-spacer></v-spacer>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-info</v-icon>
+    </v-btn>
+    <v-divider class="mx-2" inset vertical></v-divider>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-chevron-down</v-icon>
+    </v-btn>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-table</v-icon>
+    </v-btn>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-cog</v-icon>
+    </v-btn>
+    <v-divider class="mx-2" inset vertical></v-divider>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-minus</v-icon>
+    </v-btn>
+    <v-btn icon x-small>
+      <v-icon x-small>fa-times</v-icon>
+    </v-btn>
+  </v-system-bar>
+</template>
+
+<style lang="scss" scoped>
+
+#system-bar {
+  -webkit-app-region: drag;
+
+  button {
+    -webkit-app-region: no-drag;
+
+    i { margin-right: 0 !important; }
+  }
+}
+
+</style>
